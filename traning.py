fb02e70a43cc997cd2cc6941c55c9d65f23b9c9d
Gleb <Korgidgo@outlook.com>
2026-04-07 14:57:15 +0300

    Обновил код

--- traning.py
+++ traning.py
@@ -25,7 +25,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 rf = RandomForestRegressor(n_estimators=100, random_state=42)
 rf.fit(X_train, y_train)
 
-# Предсказываем для новой игры
 ризнаки = ["Платформа", "Жанр", "Год"]
 важность = rf.feature_importances_
 
