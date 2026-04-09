623d6fa70bd3f8ef2f6f51e60ab65067d4525710
Gleb <Korgidgo@outlook.com>
2026-04-09 14:48:25 +0300

    Добавил предсказание карты и победителя

--- cs2.py
+++ cs2.py
@@ -6,7 +6,33 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv(r"C:\Users\Admin\Desktop\python\cs2_all_tiers_games.csv")
 
-# 1. Сначала выбираем признаки
+features = [
+    "team1_player1_kills", "team1_player1_deaths", "team1_player1_adr","team1_player1_kast","team1_player1_kddiff",
+    "team1_player2_kills", "team1_player2_deaths", "team1_player2_adr","team1_player2_kast","team1_player2_kddiff",
+    "team1_player3_kills", "team1_player3_deaths", "team1_player3_adr","team1_player3_kast","team1_player3_kddiff",
+    "team1_player4_kills", "team1_player4_deaths", "team1_player4_adr","team1_player4_kast","team1_player4_kddiff",
+    "team1_player5_kills", "team1_player5_deaths", "team1_player5_adr","team1_player5_kast","team1_player5_kddiff",
+    "team2_player1_kills", "team2_player1_deaths", "team2_player1_adr","team2_player1_kast","team2_player1_kddiff",
+    "team2_player2_kills", "team2_player2_deaths", "team2_player2_adr","team2_player2_kast","team2_player2_kddiff",
+    "team2_player3_kills", "team2_player3_deaths", "team2_player3_adr","team2_player3_kast","team2_player3_kddiff",
+    "team2_player4_kills", "team2_player4_deaths", "team2_player4_adr","team2_player4_kast","team2_player4_kddiff",
+    "team2_player5_kills", "team2_player5_deaths", "team2_player5_adr","team2_player5_kast","team2_player5_kddiff","team1_id", "team2_id"
+]
+
+df_clean = df[features + ["map_name"]].dropna()
+
+X = df_clean[features]
+y = df_clean["map_name"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+модель_карта = RandomForestClassifier(n_estimators=100, random_state=42)
+модель_карта.fit(X_train, y_train)
+
+точность_карта = accuracy_score(y_test, модель_карта.predict(X_test))
+print(f"Точность предсказания карты: {точность_карта*100:.1f}%")
+
+
 features = [
     "team1_player1_kills", "team1_player1_deaths", "team1_player1_adr","team1_player1_kast","team1_player1_kddiff",
     "team1_player2_kills", "team1_player2_deaths", "team1_player2_adr","team1_player2_kast","team1_player2_kddiff",
@@ -20,19 +46,15 @@ features = [
     "team2_player5_kills", "team2_player5_deaths", "team2_player5_adr","team2_player5_kast","team2_player5_kddiff",
 ]
 
-# 2. Убираем пропуски
 df_clean = df[features + ["team1_win"]].dropna()
 
 X = df_clean[features]
 y = df_clean["team1_win"]
 
-# 3. Делим данные
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 4. Обучаем
-модель = RandomForestClassifier(n_estimators=100, random_state=42)
-модель.fit(X_train, y_train)
+модель_победитель = RandomForestClassifier(n_estimators=100, random_state=42)
+модель_победитель.fit(X_train, y_train)
 
-# 5. Проверяем
-точность = accuracy_score(y_test, модель.predict(X_test))
-print(f"Точность: {точность*100:.1f}%")
+точность_победитель = accuracy_score(y_test, модель_победитель.predict(X_test))
+print(f"Точность предсказания победителя: {точность_победитель*100:.1f}%")
